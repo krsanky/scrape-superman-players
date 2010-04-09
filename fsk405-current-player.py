2d36c99035308693b3d532bbfd56222863c3bf94
Paul Wisehart <paul@1ud2.com>
2010-04-09 13:50:44 -0400

    mostly works ...

--- fsk405-current-player.py
+++ fsk405-current-player.py
@@ -10,59 +10,58 @@ root = parse(fsk_url).getroot()
 tables = root.cssselect('table')
 
 #find the td 1st and then get its par
+tr = None # the <tr> that holds all of the good stuff
+for td in root.iter('td'):
+    #print td.tag + ":" + str(td.text)
+    if td.get('title'):
+        #print td.get('title')
+        if td.get('title').strip().lower() == 'ut4_superman':
+            print 'found <td title="ut4_superman" .../>'
+            tr = td.getparent().getnext()
 
-#
-#     <table cellpadding='0' cellspacing='2' style='width:160px; margin:auto; text-align:center'>
-#
-#           <tr>
-#             <td style='text-align:center' title='GAME LINK'>
-#               <div style='width:160px; white-space:nowrap; overflow:hidden; text-align:center'>
-#                 <a href='qtracker://74.221.208.117:27960?game=UrbanTerror&amp;action=show' style='text-decoration:none'>
-#                   74.221.208.117:27960
-#                 </a>
-#               </div>
-#
-#             </td>
-#           </tr>
-#
-#           <tr>
-#             <td style='text-align:center' title='FSK405 Official Superman [UAA]'>
-#               <div style='width:160px; white-space:nowrap; overflow:hidden; text-align:center'>
-#                 FSK405 Official Superman [UAA]
-#               </div>
-#             </td>
-#           </tr>
-#
-#           <tr>
-#             <td style='background-image:url(http://fsk405.com/e107_plugins/lgsl/lgsl_files/maps/urbanterror/q3ut4/ut4_superman.jpg); background-repeat:no-repeat; background-position:center'>
-#               <a href='e107_plugins/lgsl/?s=2'>
-#                 <img alt='' src='http://fsk405.com/e107_plugins/lgsl/lgsl_files/other/map_overlay.gif' style='border:none; width:160px; background:url(http://fsk405.com/e107_plugins/lgsl/lgsl_files/icons/urbanterror/urbanterror.gif); background-repeat:no-repeat; background-position:4px 4px' title='[ Type: urbanterror ] [ Game: q3ut4 ]
-#  CLICK TO VIEW SERVER DETAILS' />
-#               </a>
-#             </td>
-#           </tr>
-#
-#           <tr>
-#
-#             <td style='text-align:center' title='ut4_superman'>
-#               <div style='width:160px; white-space:nowrap; overflow:hidden; text-align:center'>
-#                 ut4_superman
-#               </div>
-#             </td>
-#           </tr>
-#           <tr>
-#             <td style='border:1px solid'>
-#               <div style='width:160px; height:76px; overflow:auto; text-align:left'>
-#
-#                 <span style='padding:1px; float:left'> PLAYERS: </span>
-#                 <span style='padding:1px; float:right'> 2 / 26 </span>
-#                 <br />
-#                 <br />
-#                   <div style='padding:1px; white-space:nowrap; overflow:hidden; text-align:left' title='Krusher'>Krusher</div>
-#                   <div style='padding:1px; white-space:nowrap; overflow:hidden; text-align:left' title='Nervous (rus)'>Nervous (rus)</div>
-#
-#               </div>
-#             </td>
-#           </tr>
-#         </table>
+#print tostring(tr)
 
+for span in tr.iter('span'):
+    print span.text.strip(),
+print
+
+## skip the 1st sub-div:
+#for div in tr.iter('div'):
+#    print div.get('title')
+#
+#print str(tr.getchildren()[0].tag)
+sub_div_1 = tr.getchildren()[0].getchildren()[0]
+#print sub_div_1.tag
+
+for div in sub_div_1.iter('div'):
+    print div.get('title')
+
+# <tr><td style="border:1px solid">
+#               <div style="width:160px; height:100px; overflow:auto; text-align:left">
+#
+#                 <span style="padding:1px; float:left"> PLAYERS: </span>
+#                 <span style="padding:1px; float:right"> 21 / 26 </span>
+#                 <br><br><div style="padding:1px; white-space:nowrap; overflow:hidden; text-align:left" title="Altair (#PoPo#)">Altair (#PoPo#)</div>
+#                   <div style="padding:1px; white-space:nowrap; overflow:hidden; text-align:left" title="Belg">Belg</div>
+#                   <div style="padding:1px; white-space:nowrap; overflow:hidden; text-align:left" title="benji9635">benji9635</div>
+#                   <div style="padding:1px; white-space:nowrap; overflow:hidden; text-align:left" title="B.V.H.YoungstarBunny">B.V.H.YoungstarBunny</div>
+#                   <div style="padding:1px; white-space:nowrap; overflow:hidden; text-align:left" title="CDaly">CDaly</div>
+#                   <div style="padding:1px; white-space:nowrap; overflow:hidden; text-align:left" title="dragonslayer">dragonslayer</div>
+#                   <div style="padding:1px; white-space:nowrap; overflow:hidden; text-align:left" title="iLLkIlLyOu">iLLkIlLyOu</div>
+#                   <div style="padding:1px; white-space:nowrap; overflow:hidden; text-align:left" title="*IOU*Apex">*IOU*Apex</div>
+#                   <div style="padding:1px; white-space:nowrap; overflow:hidden; text-align:left" title="KingFB58">KingFB58</div>
+#                   <div style="padding:1px; white-space:nowrap; overflow:hidden; text-align:left" title="LordHadshoot">LordHadshoot</div>
+#                   <div style="padding:1px; white-space:nowrap; overflow:hidden; text-align:left" title="Master(#PoPo#)">Master(#PoPo#)</div>
+#                   <div style="padding:1px; white-space:nowrap; overflow:hidden; text-align:left" title="Moving_Target">Moving_Target</div>
+#                   <div style="padding:1px; white-space:nowrap; overflow:hidden; text-align:left" title="[NBD]yo$hi">[NBD]yo$hi</div>
+#                   <div style="padding:1px; white-space:nowrap; overflow:hidden; text-align:left" title="Ozen61">Ozen61</div>
+#                   <div style="padding:1px; white-space:nowrap; overflow:hidden; text-align:left" title="Plug[Um3]">Plug[Um3]</div>
+#                   <div style="padding:1px; white-space:nowrap; overflow:hidden; text-align:left" title="[RSG]Napoleon">[RSG]Napoleon</div>
+#                   <div style="padding:1px; white-space:nowrap; overflow:hidden; text-align:left" title="Screech Primus">Screech Primus</div>
+#                   <div style="padding:1px; white-space:nowrap; overflow:hidden; text-align:left" title="ShadowLucky">ShadowLucky</div>
+#                   <div style="padding:1px; white-space:nowrap; overflow:hidden; text-align:left" title="Smerfetto">Smerfetto</div>
+#                   <div style="padding:1px; white-space:nowrap; overflow:hidden; text-align:left" title="Stabbity Stab">Stabbity Stab</div>
+#                   <div style="padding:1px; white-space:nowrap; overflow:hidden; text-align:left" title=".!verde?ben!.">.!verde?ben!.</div>
+#               </div>
+#             </td>
+#           </tr>
